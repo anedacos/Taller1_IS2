8f4f2f337238215376c5040e90252ecaf73fb76b
Jorge <jorgeluis993@hotmail.com>
2020-06-11 00:10:03 -0500

    Archivo TipoTrianguloTest para hacer los casos de prueba

---
+++ test/TipoTrianguloTest.java
@@ -0,0 +1,58 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import triangulo.TipoTriangulo;
+
+/**
+ *
+ * @author Jorge Luis
+ */
+public class TipoTrianguloTest {
+    
+
+    // TODO add test methods here.
+    // The methods must be annotated with annotation @Test. For example:
+    //
+     @Test
+     public void testEquilatero() {
+         TipoTriangulo resultado = triangulo.Triangulo.classify(2, 2, 2);
+         TipoTriangulo esperado = triangulo.TipoTriangulo.EQUILATERAL;
+         assertEquals(esperado, resultado);
+     }
+     @Test
+     public void testIsoceles() {
+         TipoTriangulo resultado = triangulo.Triangulo.classify(2, 1, 2);
+         TipoTriangulo esperado = triangulo.TipoTriangulo.ISOSCELES;
+         assertEquals(esperado, resultado);
+         
+     }
+     @Test
+     public void testEscaleno() {
+         TipoTriangulo resultado = triangulo.Triangulo.classify(2, 1, 3);
+         TipoTriangulo esperado = triangulo.TipoTriangulo.SCALENE;
+         assertEquals(esperado, resultado);
+         
+     }
+     
+      @Test
+     public void testNotATriangle() {
+         TipoTriangulo resultado = triangulo.Triangulo.classify(210, 1, 3);
+         TipoTriangulo esperado = triangulo.TipoTriangulo.NotATriangle;
+         assertEquals(esperado, resultado);
+         
+     }
+     
+      @Test
+     public void testNotATriangle2() {
+         TipoTriangulo resultado = triangulo.Triangulo.classify(10, 1, 310);
+         TipoTriangulo esperado = triangulo.TipoTriangulo.NotATriangle;
+         assertEquals(esperado, resultado);
+         
+     }
+     
+}
